f1a82e0dd6c5af7c94f62372e95cbc8a5e148647
dorukgezici <dorukgezici@gmail.com>
2017-12-16 20:33:07 +0300

    Secret file removed.

---
+++ IgniteGreenhouse/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  IgniteGreenhouse
+//
+//  Created by Doruk Gezici on 16.12.2017.
+//  Copyright © 2017 ARDIC. All rights reserved.
+//
+
+import Foundation
+
+public let UNKNOWN_ERROR = "Oops... Something went wrong."
+public let MODE = "Default"
+public let BRAND = "IgniteGreenhouse" // Also Main NodeId
---
+++ Pods/IgniteAPI/IgniteAPI/Classes/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  IgniteAPI
+//
+//  Created by Doruk Gezici on 09/07/2017.
+//  Copyright © 2017 ARDIC. All rights reserved.
+//
+
+import UIKit
+
+public let API_URL = "https://api.ardich.com/api/v3"
+public let TENANT_MAIL = "greenhouse@iot-ignite.com"
+public let BRAND = "IgniteGreenhouse"
+public let TENANT_DOMAIN = "ignite.com_greenhouse_iot-ignite.com"
+public let DEVICE_MODE = "Default"
